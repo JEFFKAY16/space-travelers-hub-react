b6c43581a6279dd7c7e3d411b1c4c396194c2832
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-03-01 16:23:19 +0100

    Setup the redux

---
+++ src/Redux/rocket/rocket.js
@@ -0,0 +1,19 @@
+const GET_ROCKET = 'spacetravelerhub/redux/rocker/GET_ROCKET';
+
+const initialState = [];
+
+export const getRocket = (rockets) => ({
+  type: GET_ROCKET,
+  payload: rockets,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ROCKET:
+      return [...action.payload];
+    default:
+      return state;
+  }
+};
+
+export default reducer;
