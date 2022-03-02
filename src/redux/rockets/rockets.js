067d3ce84937a1c0b028d71db9f3693ef156138f
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-03-02 17:56:24 +0100

    Add actions for booking and cancelling rockets and update the reducer to that effect

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,10 +1,22 @@
 const GET_ROCKET = 'spacetravelerhub/rocket/GET_ROCKET';
+const BOOK_ROCKET = 'spacetravelerhub/rocket/BOOK_ROCKET';
+const CANCEL_ROCKET = 'spacetraverhub/rocket/CANCEL_ROCKET';
 
 const initialState = [];
 
 export const getRocket = (rockets) => ({
   type: GET_ROCKET,
   payload: rockets,
+});
+
+export const bookRocket = (payload) => ({
+  type: BOOK_ROCKET,
+  payload,
+});
+
+export const cancelRocket = (payload) => ({
+  type: CANCEL_ROCKET,
+  payload,
 });
 
 export const fetchRockets = async () => {
@@ -18,6 +30,26 @@ const rocketReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKET:
       return [...action.payload];
+    case BOOK_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.rocket_id !== action.payload) {
+          return rocket;
+        }
+        return {
+          ...rocket,
+          reserved: true,
+        };
+      });
+    case CANCEL_ROCKET:
+      return state.map((rocket) => {
+        if (rocket.rocket_id === action.payload) {
+          return rocket;
+        }
+        return {
+          ...rocket,
+          reserved: false,
+        };
+      });
     default:
       return state;
   }
